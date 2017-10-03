d8c59704d1c7af8791b455789d22ed4e77495b20
JosephAlonzo <josep_alonzo@icloud.com>
2017-10-03 11:10:55 +0200

    phpfhfghgfh

---
+++ php/tp1.php
@@ -0,0 +1,11 @@
+  <form action="scriptphp.php" method="post">
+      
+        <br>
+        Nombre: <input type="text" id="valeur" >
+        <br>
+       
+        <br>
+        <input type="submit" />
+        
+
+    </form>
